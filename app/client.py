345b8536710aa06d4e8fff920df316a480afaabc
onixxxxx <offconix@gmail.com>
2024-07-28 12:45:55 +0300

    update project

--- app/client.py
+++ app/client.py
@@ -2,29 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter import filedialog
 
-class Notepad_themes:
-# Темы
-    view_colors = {
-        'dark': {
-            'text_bg': 'black', 'text_fg': 'lime', 'cursor': 'brown', 'select_bg': '#8D917A'
-        },
-        'light': {
-            'text_bg': 'white', 'text_fg': 'black', 'cursor': '#A5A5A5', 'select_bg': '#FAEEDD'
-        }
-    }
-
-    # Шрифты
-    fonts = {
-        'Arial': {
-            'font': 'Arial 14 bold'
-        },
-        'CSMS': {
-            'font': ('Comic Sans MS', 14, 'bold')
-        },
-        'TNR': {
-            'font': ('Times New Roman', 14, 'bold')
-        }
-    }
+from themes import Notepad_themes
 
 class Notepad_functions:
     def change_theme(theme):
